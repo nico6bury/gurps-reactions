6fa133f8550f5cd004f801bc98d1554a89a29514
nico6bury <nico6bury@ksu.edu>
2024-09-04 17:32:25 -0500

    fleshed out more of the structure for the gui.

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,11 @@
-use fltk::{app::App, prelude::WidgetExt, window::Window};
+use fltk::{app::App, enums::FrameType, group::Tile, menu::SysMenuBar, prelude::{GroupExt, MenuExt, WidgetExt}, window::Window};
 
+/// The width in pixels for the main window
+const WINDOW_WIDTH: i32 = 700;
+/// The height in pixels for the main window
+const WINDOW_HEIGHT: i32 = 435;
+/// The height in pixels for the top menu bar
+const TOP_MENU_HEIGHT: i32 = 35;
 /// Holds all the stuff necessary for showing and interacting with the GUI.
 #[allow(dead_code)]
 pub struct GUI {
@@ -20,8 +26,18 @@ impl GUI {
 	pub fn initialize() -> GUI {
 		let reaction_app = App::default();
 		let mut main_window = Window::default()
-			.with_size(700,435)
+			.with_size(WINDOW_WIDTH,WINDOW_HEIGHT)
 			.with_label("GURPS Reaction Rolls Helper");
+		main_window.end();
+
+		let top_menu = SysMenuBar::default()
+			.with_size(WINDOW_WIDTH, TOP_MENU_HEIGHT);
+		main_window.add(&top_menu);
+
+		let tiles = Tile::default()
+			.with_pos(0,TOP_MENU_HEIGHT)
+			.with_size(WINDOW_WIDTH, WINDOW_HEIGHT - TOP_MENU_HEIGHT);
+		main_window.add(&tiles);
 
 		main_window.show();
 		GUI {
@@ -30,3 +46,7 @@ impl GUI {
 		}//end struct construction
 	}//end initialize()
 }//end impl for GUI
+
+pub enum InterfaceMessage {
+
+}
