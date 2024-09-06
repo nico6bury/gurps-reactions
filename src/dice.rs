ac63a70eed606857adea7845c078b27b894de2fd
nico6bury <nico6bury@ksu.edu>
2024-09-06 14:07:41 -0500

    Added comments to Die code.

--- src/dice.rs
+++ src/dice.rs
@@ -4,10 +4,12 @@ use rand::Rng;
 /// This struct represents a single die.
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 struct Die {
+	/// The result from initialization.
 	pub result: i32,
 }//end struct Die
 
 impl Die {
+	/// Creates a new die with a random result between 1 and 6.
 	pub fn new() -> Die {
 		Die {
 			result: rand::thread_rng().gen_range(1..=6),
